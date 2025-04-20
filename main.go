50c3ea4c568457fe1a31382bb667ec6fe09e150c
Rob30Garcia <robertgs3011@gmail.com>
2025-04-20 00:47:31 -0300

    feat: The command line is more interactive

--- main.go
+++ main.go
@@ -1,16 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Uso: go run main.go [seu-nome]")
-		return
+	// Define uma flag chamada "uppercase"
+	uppercase := flag.Bool("uppercase", false, "Exibe a mensagem em letras maiusculas")
+
+	// Parsei as flags asntes de ler os args
+	flag.Parse()
+
+	// Pega os argumentos restantes depois das flags
+	args := flag.Args()
+
+	var name string
+
+	if len(args) > 0 {
+		name = args[0]
+	} else {
+		// Se o nome não for passado como argumento, pergunta interativamente
+		fmt.Print("Digite seu nome: ")
+		_, err := fmt.Scanln(&name)
+		if err != nil {
+			fmt.Println("Erro ao ler o nome")
+			os.Exit(1)
+		}
 	}
 
-	name := os.Args[1]
-	fmt.Printf("Hello, %s. Welcome!\n", name)
+	message := fmt.Sprintf("Hello, %s. Welcome!", name)
+
+	if *uppercase {
+		message = strings.ToUpper(message)
+	}
+
+	fmt.Println(message)
 }
