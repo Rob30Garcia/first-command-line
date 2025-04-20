099c03f89b153ae1ec92b7d1b717aec295f372e6
Rob30Garcia <robertgs3011@gmail.com>
2025-04-20 17:24:30 -0300

    feat: add cobra-cli in project

--- main.go
+++ main.go
@@ -1,53 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-	"strings"
-)
-
-// Códigos ANSI para colorir texto
-const (
-	CorVerde = "\033[32m"
-	CorAzul  = "\033[34m"
-	CorReset = "\033[0m"
-)
+import "firstcommandline/main.go/cmd"
 
 func main() {
-	// Define uma flag chamada "uppercase"
-	uppercase := flag.Bool("uppercase", false, "Exibe a mensagem em letras maiusculas")
-	fancy := flag.Bool("fancy", false, "Estiliza a saudação com cor e emoji")
-
-	// Parsei as flags asntes de ler os args
-	flag.Parse()
-
-	// Pega os argumentos restantes depois das flags
-	args := flag.Args()
-
-	var name string
-
-	if len(args) > 0 {
-		name = args[0]
-	} else {
-		// Se o nome não for passado como argumento, pergunta interativamente
-		fmt.Print("Digite seu nome: ")
-		_, err := fmt.Scanln(&name)
-		if err != nil {
-			fmt.Println("Erro ao ler o nome")
-			os.Exit(1)
-		}
-	}
-
-	message := fmt.Sprintf("Hello, %s. Welcome!", name)
-
-	if *uppercase {
-		message = strings.ToUpper(message)
-	}
-
-	if *fancy {
-		message = fmt.Sprintf("%s########## %s%s%s ##########%s", CorAzul, CorVerde, message, CorAzul, CorReset)
-	}
-
-	fmt.Println(message)
+	cmd.Execute()
 }
