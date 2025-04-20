2c5a8af7143c22b2c99a6081ec6bc221449d0817
Rob30Garcia <robertgs3011@gmail.com>
2025-04-20 17:43:35 -0300

    feat: add greet subcommand in project with cobra-cli

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# first-command-line
+# First Commando Line
+
+### Commands to use - CLI with Cobra in Go
+
+- hello command
+```bash
+    go run main.go hello <your_name>
+```
+- Use --uppercase flag
+```bash
+    go run main.go hello <your_name> --uppercase
+or
+    go run main.go hello --uppercase <your_name>
+```
+
+- Execute subcommand greet
+```bash
+    go run main.go hello greet <your_name>
+```
---
+++ cmd/greet.go
@@ -0,0 +1,44 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// greetCmd represents the greet command
+var greetCmd = &cobra.Command{
+	Use:   "greet",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 {
+			fmt.Printf("Quem você quer cumprimentar?")
+			return
+		}
+		name := args[0]
+		fmt.Printf("Olá, %s! Bem-vindo!\n", name)
+	},
+}
+
+func init() {
+	helloCmd.AddCommand(greetCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// greetCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// greetCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
